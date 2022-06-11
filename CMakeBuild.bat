cc3a9b372b1b440385bc60c148c3dcba9ea5e524
Tim <15017472+doodlum@users.noreply.github.com>
2022-06-11 20:06:51 +0100

    Update build file

--- CMakeBuild.bat
+++ CMakeBuild.bat
@@ -8,6 +8,7 @@ cmake --build build/skyrim --config Release
 if %ERRORLEVEL% NEQ 0 exit 1
 
 xcopy "build\skyrim\release\*.dll" "dist\SKSE\Plugins\" /I /Y
+xcopy "build\skyrim\release\*.pdb" "dist\SKSE\Plugins\" /I /Y
 
 xcopy "package" "dist" /I /Y /E
 
